41d4218ca9182923d450892b15aa1dc599e72cec
markv58 <markvittes@gmail.com>
2019-04-14 23:47:00 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,32 +2,36 @@
 
 pip3 install -r requirements.txt --user
 sleep 10s
-mkdir ~/.polyglot/nodeservers/PiCamMonitor/Scripts
-cp -a ~/.polyglot/nodeservers/PiCamMonitor/ScriptTemplates/. ~/.polyglot/nodeservers/PiCamMonitor/Scripts/
+if [ ! -d "/home/pi/.polyglot/nodeservers/PiCamMonitor/Scripts" ]; then
+  mkdir ~/.polyglot/nodeservers/PiCamMonitor/Scripts
+  cp -a ~/.polyglot/nodeservers/PiCamMonitor/ScriptTemplates/. ~/.polyglot/nodeservers/PiCamMonitor/Scripts/
+fi
 sleep 3s
 sudo chmod +x -R ~/.polyglot/nodeservers/PiCamMonitor/Scripts # activate the scripts
 sleep 2s
-mkdir /home/pi/Pictures/Family
-mkdir /home/pi/Pictures/Friends
-mkdir /home/pi/Pictures/Kids
-mkdir /home/pi/Pictures/Vacation
-mkdir /home/pi/Pictures/Christmas
-mkdir /home/pi/Pictures/Valentines
-mkdir /home/pi/Pictures/StPatricks
-mkdir /home/pi/Pictures/Easter
-mkdir /home/pi/Pictures/YomKippur
-mkdir /home/pi/Pictures/Halloween
-mkdir /home/pi/Pictures/Thanksgiving
-mkdir /home/pi/Pictures/NewYears
-mkdir /home/pi/Pictures/Birthday1
-mkdir /home/pi/Pictures/Birthday2
-mkdir /home/pi/Pictures/Birthday3
-mkdir /home/pi/Pictures/Birthday4
-mkdir /home/pi/Pictures/Birthday5
-mkdir /home/pi/Pictures/User1
-mkdir /home/pi/Pictures/User2
-mkdir /home/pi/Pictures/User3
-mkdir /home/pi/Pictures/User4
-sleep 2s
-find /home/pi/Pictures -type d -exec cp -i /home/pi/.polyglot/nodeservers/PiCamMonitor/Images/sample.jpg {} \;
 
+if [ ! -d "/home/pi/Pictures/Family" ]; then
+  mkdir /home/pi/Pictures/Family
+  mkdir /home/pi/Pictures/Friends
+  mkdir /home/pi/Pictures/Kids
+  mkdir /home/pi/Pictures/Vacation
+  mkdir /home/pi/Pictures/Christmas
+  mkdir /home/pi/Pictures/Valentines
+  mkdir /home/pi/Pictures/StPatricks
+  mkdir /home/pi/Pictures/Easter
+  mkdir /home/pi/Pictures/YomKippur
+  mkdir /home/pi/Pictures/Halloween
+  mkdir /home/pi/Pictures/Thanksgiving
+  mkdir /home/pi/Pictures/NewYears
+  mkdir /home/pi/Pictures/Birthday1
+  mkdir /home/pi/Pictures/Birthday2
+  mkdir /home/pi/Pictures/Birthday3
+  mkdir /home/pi/Pictures/Birthday4
+  mkdir /home/pi/Pictures/Birthday5
+  mkdir /home/pi/Pictures/User1
+  mkdir /home/pi/Pictures/User2
+  mkdir /home/pi/Pictures/User3
+  mkdir /home/pi/Pictures/User4
+  sleep 2s
+  find /home/pi/Pictures -type d -exec cp -i /home/pi/.polyglot/nodeservers/PiCamMonitor/Images/sample.jpg {} \;
+fi
